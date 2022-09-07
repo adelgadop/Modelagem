14aae0ab301bbeb14299bd88a2d7431f4bbddc74
Alejandro Delgado <adelgado@iag.usp.br>
2022-09-07 09:29:45 -0300

    Recently improving

--- Exercicio_1/functions.py
+++ Exercicio_1/functions.py
@@ -3,20 +3,30 @@ import matplotlib.pyplot as plt
 
 def sol_analytical(cond_front, fun, X, U, T, Nx):
     
-    if cond_front == "radiacional":
+    if cond_front == "fixa":
         if fun == "Gaussiana":
-            C = {nr:10*np.exp(-(X - U*T - 51*5000)**2/(nr*5000)**2) for nr in [10, 2]}
+            sol = {nr:10*np.exp(-(X - U*T - 51*5000)**2/(nr*5000)**2) for nr in [10, 2]}
 
         if fun == "Retângulo":
-            rect = np.linspace(-10, 10, 101)
-            C = {np.where(abs(np.linspace(-10, 10, 101)) <= 0.5, 1, 0)*nr for nr in [10, 2]}
+            sol = {np.where(abs(np.linspace(-10, 10, Nx)) <= 0.5, 1, 0)*nr for nr in [10, 2]}
+
+    elif cond_front == "radiacional":
+        if fun == "Gaussiana":
+            sol = {nr:10*np.exp(-(X - U*T - 51*5000)**2/(nr*5000)**2) for nr in [10, 2]}
+
+        if fun == "Retângulo":
+            sol = {np.where(abs(np.linspace(-10, 10, Nx)) <= 0.5, 1, 0)*nr for nr in [10, 2]}
        
     elif cond_front == "periódica":
-        C = {nr:np.maximum(10*np.exp(-(X - U*T - 51*5000)**2/(nr*5000)**2),
-             10*np.exp(-(X - U*T - (51-(Nx-2))*5000)**2/(nr*5000)**2))
-             for nr in [10, 2]}
+        if fun == "Gaussiana":
+            sol = {nr:np.maximum(10*np.exp(-(X - U*T - 51*5000)**2/(nr*5000)**2),
+                   10*np.exp(-(X - U*T - (51-(Nx-2))*5000)**2/(nr*5000)**2)) for nr in [10, 2]}
 
-    return C
+        if fun == "Retângulo":
+            pass
+
+
+    return sol
     
 def Conc(i, nr):
     """
@@ -35,11 +45,15 @@ def sol_num(aprox, cond_front, fun, CFL, nr, Nx, Nt, x, dx, U, t):
     hora        : Tempo em horas (int)
     """
     C = np.zeros((Nx, Nt))   # Matriz
+    # Space: the last is Nx-1 (e.g., not use Nx)
+    # Time: the last in Nt-1 (e.g., not use n+1)
+    u = np.zeros((Nx, Nt))
 
     # Condições iniciais
     # -------------------
     if fun == "Gaussiana":
         C[:,0] = Conc(x/dx, nr)
+        u[:,0] = Conc(x/dx, nr)
 
     elif fun == "Retângulo":
         C[[int(51 - 2/2), 51, int(51 + 2/2)], 0] = 1/2*nr
@@ -47,37 +61,80 @@ def sol_num(aprox, cond_front, fun, CFL, nr, Nx, Nt, x, dx, U, t):
     for n in range(1, Nt): 
         # Aproximação numérica:
         # ---------------------
-        if aprox == "ordem1":
-            C[1:Nx, n] = C[1:Nx, n-1] - CFL*(C[1:Nx, n-1]- C[0:Nx-1, n-1])
+        if aprox == "ordem1": # ok
 
-        elif aprox == "leapfrog":
-            # Primeiro passo: Forward Time Central Space
-            C[1:Nx-1, 1] = C[1:Nx-1, 0] - CFL/2*(C[2:Nx, 0] - C[0:Nx-2, 0])
-            C[0, 1]      = Conc(x[0]/dx - U*t[1], nr)
-            Q = (1 - CFL)/(1 + CFL)
-            C[1:Nx-1, n] = C[1:Nx-1, n - 2] - CFL*(C[2:Nx, n-1] - C[0:Nx-2, n-1])
+            # Condição de fronteira
+            # ---------------------
+            if cond_front == 'fixa':
+                C[1:Nx, n] = C[1:Nx, n-1] - CFL*(C[1:Nx, n-1]- C[0:Nx-1, n-1]) 
+                C[[0,-1], n] = [0,0]
 
-            # Boundary condition
-            C[0, n]      = Conc((x[0] - U*t[n])/dx, nr)    
-            C[Nx-1, n]   = C[Nx - 2, n-1] - Q*C[Nx - 2, n] + Q*C[Nx-1, n-1]
-
-        elif aprox == "Matsuno":
-            pass
-
-                    
-        # Condição de fronteira
-        # ---------------------
-        if cond_front == 'fixa':
-            C[[0,-1], n-1] = [0,0]
-
-        elif cond_front == 'periódica':
-            C[[0, 1], n] = C[[-1,-2],n-1]
+            elif cond_front == 'periódica':
+                C[1:Nx, n] = C[1:Nx, n-1] - CFL*(C[1:Nx, n-1]- C[0:Nx-1, n-1]) 
+                C[[0,1], n] = C[[Nx-2,Nx-1], n-1] - CFL*(C[[Nx-2,Nx-1], n-1]- C[[Nx-3,Nx-2], n-1]) 
                 
-        elif cond_front == 'radiacional':
-            C[ 0, n] = C[ 0, n-1] - CFL*(C[ 1, n-1]- C[ 0, n-1])
-            C[-1, n] = C[-1, n-1] - CFL*(C[-1, n-1]- C[-2, n-1])
+            elif cond_front == 'radiacional':
+                C[1:Nx, n] = C[ 1:Nx, n-1] - CFL*(C[1:Nx, n-1]- C[0:Nx-1, n-1])
+
+        # -------------------------------------------------
+        elif aprox == "leapfrog":  # ok
+
+            if n ==1:
+            # Euler-forward scheme for the first time step (chapter 4.2, Doos 2020)
+                C[1:-1,n] = C[1:-1,n-1] - CFL*(C[2:Nx,n-1] - C[1:-1,n-1])   # rev ok
+
+            elif n > 1:
+                C[1:-1, n] = C[1:-1, n - 2] - CFL*(C[2:Nx, n-1] - C[0:-2, n-1])   # rev ok
             
-    
+            # Condição de fronteira
+            # ---------------------
+            if cond_front == 'fixa': # ok
+                C[[0,-1], n] = [0,0]
+            
+            elif cond_front == 'periódica': # ok
+                C[ 0, n] = C[100, n-2] - CFL*(C[1, n-1] - C[-1, n-1])
+                C[ 1, n] = C[ 1, n-2] - CFL*(C[2, n-1] - C[0, n-1])
+                #C[ 2, n] = C[ 2, n-2] - CFL*(C[3, n-1] - C[1, n-1])
+                C[-1, n] = C[ 99, n-2] - CFL*(C[100, n-1] - C[98, n-1])
+
+            elif cond_front == 'radiacional': # ok
+                C[ 0, n] = C[ 0, n-1] - CFL*(C[ 1, n-1]- C[ 0, n-1])
+                C[-1, n] = C[-1, n-1] - CFL*(C[-1, n-1]- C[-2, n-1]) 
+       
+       # -------------------------------------------------
+        elif aprox == "ordem4":
+                      
+            C[2:-2, n] = C[2:-2, n-1] - CFL*(4/3*(C[3:-1, n-1] - C[1:-3, n-1])/2 - 1/3*(C[4:, n-1] - C[:-4, n-1])/4) 
+            
+            # Condição de fronteira
+            # ---------------------
+            if cond_front == 'fixa':
+                C[[0,-1], n] = [0,0]
+            
+            elif cond_front == 'periódica': # rev ok
+                C[ 2, n] = C[  2, n-1] - CFL*(4/3*(C[  3, n-1] - C[  1, n-1])/2 - 1/3*(C[4, n-1] - C[ 0, n-1])/4)
+                C[ 1, n] = C[  1, n-1] - CFL*(4/3*(C[  2, n-1] - C[  0, n-1])/2 - 1/3*(C[3, n-1] - C[100, n])/4)
+                C[ 0, n] = C[100, n-1] - CFL*(4/3*(C[  1, n-1] - C[ -1, n-1])/2 - 1/3*(C[2, n-1] - C[98, n-1])/4)
+                C[-1, n] = C[ 99, n-1] - CFL*(4/3*(C[100, n-1] - C[ 98, n-1])/2 - 1/3*(C[1, n-1] - C[97, n-1])/4) 
+                C[-2, n] = C[ 98, n-1] - CFL*(4/3*(C[ 99, n-1] - C[ 97, n-1])/2 - 1/3*(C[100, n-1] - C[ 96, n-1])/4)
+
+            elif cond_front == 'radiacional':
+                C[ 0, n] = C[ 0, n-1] - CFL*(C[ 1, n-1]- C[ 0, n-1])
+                C[-1, n] = C[-1, n-1] - CFL*(C[-1, n-1]- C[-2, n-1])
+
+        # -------------------------------------------------       
+        elif aprox == "Matsuno": # somente periódica 
+
+            u[1:-1,n] = C[1:-1, n-1] - CFL*(C[1:-1, n-1]- C[:-2, n-1])
+            C[1:-1,n] = C[1:-1, n-1] - CFL*(u[1:-1, n-1] - u[:-2,n-1])
+
+            if cond_front == 'periódica':
+               u[0, n] = C[-1, n-1] - CFL*(C[-1, n-1]- C[-2, n-1])
+               C[0,n] = C[0, n-1] - CFL*(u[-1, n-1] - u[-2,n-1])
+               u[-1, n] = C[-1, n-1] - CFL*(C[-1, n-1]- C[-2, n-1])
+               C[-1,n] = C[-1, n-1] - CFL*(u[-1, n-1] - u[-2,n-1])
+ 
+
     return C
     
 def plot_sol_num(fun, aprox, cond_front, ylabel, dP, hora, Nx, Nt, CFL, U, x, dx, t, dt):
@@ -94,7 +151,7 @@ def plot_sol_num(fun, aprox, cond_front, ylabel, dP, hora, Nx, Nt, CFL, U, x, dx
 
         ax.plot(C[:,-1], color='g', lw=3, label = f"Final {hora} horas ")
         ax.set_ylabel(ylabel)
-        ax.set_title("Concentração"+f" para $\Delta$t = {dt.round(0)} segundos, $\Delta$x = {dx} metros.",loc='left')
+        ax.set_title("Concentração"+f" para $\Delta$t = {dt.round(0)} segundos, $\Delta$x = {dx} metros, CFL = {CFL}.",loc='left')
         ax.legend(fontsize=8, ncol=2)
         ax.set_xlabel("Pontos da grade")
         ax.text(2,3, r"$\vec U$ = "+ f"{U} m/s.", fontsize=12)
@@ -116,7 +173,7 @@ def plot_sol_num(fun, aprox, cond_front, ylabel, dP, hora, Nx, Nt, CFL, U, x, dx
 
             ax[j].plot(C[:,-1], color='g', lw=3, label = f"Final {hora} horas ")
             ax[j].set_ylabel(ylabel)
-            ax[j].set_title("Concentração"+f" para $\Delta$t = {dt.round(0)} segundos, $\Delta$x = {dx} metros e nr = {nr}.",loc='left')
+            ax[j].set_title("Concentração"+f" para $\Delta$t = {dt.round(0)} segundos, $\Delta$x = {dx} metros, CFL = {CFL.round(2)} e nr = {nr}.",loc='left')
             ax[0].legend(fontsize=8, ncol=2)
             ax[1].set_xlabel("Pontos da grade")
             ax[1].text(2,3, r"$\vec U$ = "+ f"{U} m/s.", fontsize=12)
--- README.md
+++ README.md
@@ -3,16 +3,26 @@ Exercicíos desenvolvidos em Python como parte dos estudos de doutorado em meteo
 
 ## Exercicío 1
 - [x] Resolver problema de transporte de um pulso inicial de poluição numa situação em que o vento seja constante.
-   - [x] Desenvolver as soluções numéricas: Malha de espaçamento, condição inicial, U = 10 m/s. Integrar durante o tempo suficiente para que perturbação inicial volte para a parte central da malha no caso da fronteira; demais casos, integrar até que a perturbação chegue totalmente na fronteira.
+   - [x] Desenvolver as soluções numéricas: Malha de espaçamento, condição inicial, U = 10 m/s. Integrar durante o tempo suficiente para que perturbação inicial volte para a parte central da malha no caso da fronteira periódica; demais casos, integrar até que a perturbação chegue totalmente na fronteira.
+   - [x] Solução analítica para função Gaussiana
+   - [ ] Solução analítica para função retângulo.
    - [ ] Método de solução
      1. [x] Inicie com um esquema de ordem 1 (cuidado com a estabilidade do esquema na escolha do delta t na derivada espacial).
+        1. [x] Periódica (ok)
      2. [x] Compare a solução dada em 1 com um esquema leapfrog de ordem 2 (espaço e tempo)
-     3. [ ] Modifique a derivada no espaço para um esquema de 4a ordem (será necessário manter a segunda ordem no ponto imediatamente vizinho da fronteira no caso da CF fixa).
-     4. [ ] Use também um método iterativo, por exemplo, o `Esquema Matsuno`.
+        1. [x] Fixa (ok)
+        2. [x] Periódica (ok)
+        3. [x] Radiacional (ok)
+     3. [x] Modifique a derivada no espaço para um esquema de 4a ordem (será necessário manter a segunda ordem no ponto imediatamente vizinho da fronteira no caso da CF fixa).
+        1. [x] Periódica
+     4. [x] Use também um método iterativo, por exemplo, o `Esquema Matsuno`.
+        1. [x] Periódica
      5. [ ] Use um `método implícito` (e.g., Crank-Nicolson scheme).
+        1. [ ] Periódica
      6. [ ] Finalmente, use um Runge Kutta de 4ª ordem no tempo com um esquema de 4to ordem no espaço
+        1. [ ] Periódica
      
-     Obs: quanto a CF, não é necessário fazer os 3 casos para todos os experimentos. Faça apenas para o caso Leapfrog. Nos demais, faça sempre a CF periódica.
+     Obs: quanto a CF, não é necessário fazer os 3 casos (fixa, radiacional e periódica) para todos os experimentos. Faça apenas para o caso Leapfrog. Nos demais, faça sempre a CF periódica.
      
 - [ ] Apresentar o exercício na forma de um "paper": introdução, metodologia, resultados e discussão dos resultados.
      
